4de604fb8491813d20f44bbffee0b4e4f0e94512
ngule <ngule@DESKTOP-PVEILPL>
2023-08-01 16:58:44 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _iProductDal;
+
+        public ProductManager(IProductDal iProductDal)
+        {
+            _iProductDal = iProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+           return _iProductDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryID(int id)
+        {
+            return _iProductDal.GetAll(p=>p.CategoryID == id);
+        }
+
+        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        {
+            return _iProductDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryID(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
